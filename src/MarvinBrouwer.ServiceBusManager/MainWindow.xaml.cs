315faf5bf80ce7528d7a7a76341d9433f481e727
marvin.brouwer <marvin.brouwer@valtech.com>
2022-07-08 16:27:03 +0200

    [OTHER] Added some event handlers

--- src/MarvinBrouwer.ServiceBusManager/MainWindow.xaml.cs
+++ src/MarvinBrouwer.ServiceBusManager/MainWindow.xaml.cs
@@ -11,4 +11,49 @@ public partial class MainWindow : Window
 	{
 		InitializeComponent();
 	}
+
+	private void ShowOpenConfigurationDialog(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ReloadConfiguration(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void OpenDownloadFolder(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void OpenGitHubRepository(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ShowReadmeWindow(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ShowRequeueDialog(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ShowDownloadDialog(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ShowUploadDialog(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ShowClearDialog(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
 }
